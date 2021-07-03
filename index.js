8ce9600e45af98740623312473a5e09bab6def40
tr-404 <ff4aa1@gmail.com>
2021-07-03 13:55:54 +0400

    fixes

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const github = require('@actions/github');
 
 try {
   const data = core.getInput('data');
-  console.log(`Data: ${JSON.stringify(data)}`);
+  console.log(`Data: ${data}`);
 
   core.setOutput("data", data);
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
